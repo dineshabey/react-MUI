ea657aca7067f8be003e05b3ca5e5743a6e29558
Dinesh <dinabeysinghe@gmail.com>
2023-10-18 11:25:40 +0530

    add dialog box and heading

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import BasicButtons from './components/BasicButtons';
 import LoadingButtonsTransition from './components/LoadingButtonsTransition';
 import CustomizedRating from './components/CustomizedRating';
 import SelectOtherProps from './components/SelectOtherProps';
+import BasicTextFields from './components/BasicTextFields';
+import Types from './components/Types';
+import ScrollDialog from './components/ScrollDialog';
 
 function App() {
   return (
@@ -13,6 +16,11 @@ function App() {
       <LoadingButtonsTransition />
       <CustomizedRating />
       <SelectOtherProps />
+      <BasicTextFields />
+      <br />
+      <Types />
+      <br />
+      <ScrollDialog />
     </div>
   );
 }
