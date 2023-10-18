6d6453f856eaa037434eb6b0ef62478463314a8a
Dinesh <dinabeysinghe@gmail.com>
2023-10-18 20:12:09 +0530

    add hover effect - GRID

--- src/layouts/LearnGrid.js
+++ src/layouts/LearnGrid.js
@@ -8,40 +8,49 @@ export default function LearnGrid() {
         container
         sx={{ bgcolor: 'teal' }}
         spacing={3}
-        justifyContent='space-between'
+        justifyContent='space-around'
         >
         <Grid item>
-          <Box sx={{ bgcolor: 'lightgrey' }}>
+          <Box sx={{ 
+            bgcolor: 'lightgrey',
+            p:4,
+            '&:hover':{bgcolor:'white'},
+             }}>
             <Typography>Item 01</Typography>
 
           </Box>
         </Grid>
         <Grid item>
-          <Box sx={{ bgcolor: 'lightgrey' }}>
+          <Box sx={{ bgcolor: 'lightgrey',p:4 ,
+           '&:hover':{bgcolor:'red'},}}>
             <Typography>Item 02</Typography>
 
           </Box>
         </Grid>
         <Grid item>
-          <Box sx={{ bgcolor: 'lightgrey' }}>
+          <Box sx={{ bgcolor: 'lightgrey',p:4,
+           '&:hover':{bgcolor:'yellow'}, }}>
             <Typography>Item 03</Typography>
 
           </Box>
         </Grid>
         <Grid item>
-          <Box sx={{ bgcolor: 'lightgrey' }}>
+          <Box sx={{ bgcolor: 'lightgrey',p:4 ,
+           '&:hover':{bgcolor:'orange'},}}>
             <Typography>Item 04</Typography>
 
           </Box>
         </Grid>
         <Grid item>
-          <Box sx={{ bgcolor: 'lightgrey' }}>
+          <Box sx={{ bgcolor: 'lightgrey',p:4 ,
+           '&:hover':{bgcolor:'black'},}}>
             <Typography>Item 05</Typography>
 
           </Box>
         </Grid>
         <Grid item>
-          <Box sx={{ bgcolor: 'lightgrey' }}>
+          <Box sx={{ bgcolor: 'lightgrey',p:4 ,
+           '&:hover':{bgcolor:'blue'},}}>
             <Typography>Item 06</Typography>
 
           </Box>
