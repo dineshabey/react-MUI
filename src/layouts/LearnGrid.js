261cba247ff69a193d2dae142393221149a93e18
Dinesh <dinabeysinghe@gmail.com>
2023-10-18 18:42:07 +0530

    create basic GRID view

--- src/layouts/LearnGrid.js
+++ src/layouts/LearnGrid.js
@@ -1,11 +1,47 @@
-import { Grid } from '@mui/material'
+import { Box, Grid, Typography } from '@mui/material'
 import React from 'react'
 
 export default function LearnGrid() {
   return (
     <div>
-      <Grid>
-        
+      <Grid container sx={{bgcolor:'teal'}}>
+        <Grid item>
+          <Box sx={{ bgcolor: 'lightgrey' }}>
+            <Typography>Item 01</Typography>
+
+          </Box>
+        </Grid>
+        <Grid item>
+          <Box sx={{ bgcolor: 'lightgrey' }}>
+            <Typography>Item 02</Typography>
+
+          </Box>
+        </Grid>
+        <Grid item>
+          <Box sx={{ bgcolor: 'lightgrey' }}>
+            <Typography>Item 03</Typography>
+
+          </Box>
+        </Grid>
+        <Grid item>
+          <Box sx={{ bgcolor: 'lightgrey' }}>
+            <Typography>Item 04</Typography>
+
+          </Box>
+        </Grid>
+        <Grid item>
+          <Box sx={{ bgcolor: 'lightgrey' }}>
+            <Typography>Item 05</Typography>
+
+          </Box>
+        </Grid>
+        <Grid item>
+          <Box sx={{ bgcolor: 'lightgrey' }}>
+            <Typography>Item 06</Typography>
+
+          </Box>
+        </Grid>
+
       </Grid>
     </div>
   )
