8b9111940e7d55d091d0bac5cc1ae959f22a8cf1
Dinesh <dinabeysinghe@gmail.com>
2023-10-18 18:19:49 +0530

    interative row-coloum

--- src/layouts/BasicGrid.js
+++ src/layouts/BasicGrid.js
@@ -4,6 +4,7 @@ import Box from '@mui/material/Box';
 import Paper from '@mui/material/Paper';
 import Grid from '@mui/material/Grid';
 import { Typography } from '@mui/material';
+import { Directions } from '@mui/icons-material';
 
 const Item = styled(Paper)(({ theme }) => ({
   backgroundColor: 'skyblue',
@@ -17,15 +18,24 @@ const Item = styled(Paper)(({ theme }) => ({
 export default function BasicGrid() {
   return (
     <Box sx={{ flexGrow: 1 }}>
-      <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
+      <Grid container
+        spacing={{ xs: 2, md: 3 }}
+        columns={{ xs: 4, sm: 8, md: 12 }}
+        directions="column"
+        justifyContent="space-between"
+      >
         {Array.from(Array(6)).map((_, index) => (
           <Grid item xs={2} sm={4} md={6} key={index}>
             <Item>
-              <Typography sx={{ fontSize: { xs: '20px', sm: '30px', md: '40px',lg:'100px' } }}>xs=2</Typography>
+              <Typography sx={{ fontSize: { xs: '20px', sm: '30px', md: '40px', lg: '100px' } }}>
+              xs=2 - {index+1}
+              </Typography>
             </Item>
           </Grid>
         ))}
       </Grid>
-    </Box>
+    </Box >
+
+
   );
 }
