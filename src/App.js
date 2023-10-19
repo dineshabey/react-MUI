3cdbffda1df4457699996125d234877febe4db6e
Dinesh <dinabeysinghe@gmail.com>
2023-10-19 15:54:33 +0530

    add basic row & col  - GRID

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ function App() {
 
       {/* <BasicGrid/> */}
 
-      <LearnGrid />
+      {/* <LearnGrid /> */}
 
       <NestedGrid/>
 
--- src/layouts/NestedGrid.js
+++ src/layouts/NestedGrid.js
@@ -5,10 +5,16 @@ export default function NestedGrid() {
     return (
         <div>
             <Grid container>
-                <Grid item>
+                <Grid item xs={5} sm={4} md={2} sx={{ bgcolor: 'red' }}>
                     <Typography>
-                        Nested Grid
+
                     </Typography>
+                </Grid>
+                <Grid item xs={7} sm={8} md={10} sx={{ bgcolor: 'yellow' }}>
+                    Colum 2
+                </Grid>
+                <Grid item>
+                    Colum 3
                 </Grid>
 
             </Grid>
